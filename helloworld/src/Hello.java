efbe47e525adcd59acbe5598e3158681c39c71fc
songxc <1580441073@qq.com>
2019-05-29 16:54:18 +0800

    helloworld

---
+++ helloworld/src/Hello.java
@@ -0,0 +1,11 @@
+
+/**
+ * @Author: Songxc
+ * @Date: 2019/5/29 16:42
+ * @Version 1.0
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
