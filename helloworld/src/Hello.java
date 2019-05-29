c193167ca177346d60975ac921ef804c186439a1
songxc <1580441073@qq.com>
2019-05-29 17:11:28 +0800

    Hello Git

--- helloworld/src/Hello.java
+++ helloworld/src/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Github");
+		System.out.println("Hello Git");
 	}
 }
